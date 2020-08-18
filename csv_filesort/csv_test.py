6bcb661c06293e15015f6479640ceb7d92a92be8
David Buzzell <xhamyd@gmail.com>
2020-08-17 22:26:06 -0400

    README complete! (#2)

--- csv_filesort/csv_test.py
+++ csv_filesort/csv_test.py
@@ -1,24 +1,24 @@
-import os, sys
 import csv
+import os
 
 ROOT_PATH = "/home/dbuzzell/test_dir"
 
 
-def getFiles(root_name, listFiles, indent):
-  indentStr = [""] * indent
-  if os.path.isfile(root_name):
-    return [indentStr + [root_name]]
-  else:
-    file_sort = sorted(sorted(os.listdir(root_name), key=lambda s: s.casefold()), key=lambda s: os.path.isfile(root_name + "/" + s))
-    for f in file_sort:
-      new_entry = indentStr + [f + "/"] if os.path.isdir(root_name + "/" + f) else indentStr + [f]
-      listFiles.append(new_entry)
-      getFiles("{}/{}".format(root_name, f), listFiles, indent + 1)
-    return listFiles
+def getFiles(root_name, list_files, indent):
+    indent_str = [""] * indent
+    if os.path.isfile(root_name):
+        return [indent_str + [root_name]]
+    else:
+        file_sort = sorted(sorted(os.listdir(root_name), key=lambda s: s.casefold()),
+                           key=lambda s: os.path.isfile(root_name + "/" + s))
+        for f in file_sort:
+            new_entry = indent_str + [f + "/"] if os.path.isdir(root_name + "/" + f) else indent_str + [f]
+            list_files.append(new_entry)
+            getFiles("{}/{}".format(root_name, f), list_files, indent + 1)
+        return list_files
 
   
 listofFiles = getFiles(ROOT_PATH, [[ROOT_PATH + "/"]], 1)
 with open("testing.csv", "w") as csvFile:
-  writer = csv.writer(csvFile)
-  writer.writerows(listofFiles)
-  
+    writer = csv.writer(csvFile)
+    writer.writerows(listofFiles)
