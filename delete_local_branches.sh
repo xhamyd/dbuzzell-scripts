3e1df00da45470ee62f45adeb99479cfbb3d18e8
David Buzzell <dbuzzell@dspconcepts.com>
2020-09-03 15:36:51 -0400

    Edits needed for real-world deployment

--- delete_local_branches.sh
+++ delete_local_branches.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# TODO: Switch to a "text-adventure" style setup
+# ex.
+# This branch (dbuzzell/ABC-123-blah) is gone on master
+# Delete? (Y/[n]) _
+# Are you sure (it's not fully merged, need to use `git branch -D`)? _
+
 if [[ -z "$1" || "$1" == 1 ]]; then
     # Option 1: Remove tracking branches not on origin
     git remote prune origin
@@ -8,11 +14,12 @@ elif [[ "$1" == 2 ]]; then
     # Source: https://stackoverflow.com/a/38404202
     git checkout master
     git fetch -p
-    git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -d
+    git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
 elif [[ "$1" == 3 ]]; then
     # Option 3: Choose which merged branches to delete
     # Source: https://stackoverflow.com/a/28464339
     git branch --merged > /tmp/merged-branches
+    # TODO: nano isn't installed on Windows
     nano /tmp/merged-branches  # Remove branch names not to be deleted
     xargs git branch -d < /tmp/merged-branches
 else
