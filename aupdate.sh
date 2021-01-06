4c2ef2461516d4de443f4803c78edf0de30a38df
David Buzzell <dbuzzell@dspconcepts.com>
2021-01-06 08:38:49 -0500

    full-upgrade >>> upgrade

--- aupdate.sh
+++ aupdate.sh
@@ -3,14 +3,15 @@
 sudo true  # Blank command for the password prompt
 echo "APT updates..."
 sudo apt -qq update -y
-sudo apt -qq upgrade -y
-#sudo apt -qq full-upgrade -y
+#sudo apt -qq upgrade -y  # full-upgrade does more, learned from the RPi community
+sudo apt -qq full-upgrade -y
 #sudo apt -qq autoclean -y
 #sudo apt -qq clean -y
 sudo apt -qq autoremove -y
 echo ""
-echo "Upgrading pip..."
-pip -q install --upgrade pip
-echo ""
+#@NOTE: Everyone's pip setup is different, not automatically doing this
+# echo "Upgrading pip..."
+# pip -q install --upgrade pip
+# echo ""
 echo "Update snaps..."
 sudo snap refresh
