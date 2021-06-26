149b3b352136366cd423027d184fa35d39be3c29
David Buzzell <xhamyd@gmail.com>
2021-06-26 18:46:47 -0400

    LPMK2 <=> SenseHat done!

---
+++ launchpad_colors.py
@@ -0,0 +1,132 @@
+from recordclass import recordclass
+
+VelColor = recordclass("VelColor", "red green blue")
+
+LAUNCHPAD_COLORS = [
+    VelColor(  0,   0,   0),  #   0: off
+    VelColor( 28,  28,  28),
+    VelColor(124, 124, 124),  #   2: gray
+    VelColor(252, 252, 252),  #   3: white
+    VelColor(255,  74,  71),    VelColor(255,  10,   0),  #   5: red    VelColor( 90,   1,   0),
+    VelColor( 25,   0,   0),
+    VelColor(255, 189,  98),
+    VelColor(255,  86,   0),
+    VelColor( 90,  29,   0),  #  10
+    VelColor( 36,  24,   0),
+    VelColor(253, 253,  33),
+    VelColor(253, 253,   0),
+    VelColor( 88,  88,   0),
+    VelColor( 24,  24,   0),
+    VelColor(128, 253,  42),
+    VelColor( 64, 253,   0),
+    VelColor( 22,  88,   0),
+    VelColor( 19,  40,   0),
+    VelColor( 52, 253,  43),  #  20
+    VelColor(  0, 253,   0),  #  21: green
+    VelColor(  0,  88,   0),
+    VelColor(  0,  24,   0),
+    VelColor( 51, 253,  70),
+    VelColor(  0, 253,   0),  #  25: green
+    VelColor(  0,  88,   0),
+    VelColor(  0,  24,   0),
+    VelColor( 50, 253, 126),
+    VelColor(  0, 253,  58),
+    VelColor(  0,  88,  20),  #  30
+    VelColor(  0,  28,  15),
+    VelColor( 47, 252, 176),
+    VelColor(  0, 252, 145),
+    VelColor(  0,  88,  49),
+    VelColor(  0,  24,  15),
+    VelColor( 57, 191, 255),
+    VelColor(  0, 167, 255),
+    VelColor(  0,  64,  81),
+    VelColor(  0,  16,  24),
+    VelColor( 65, 134, 255),  #  40
+    VelColor(  0,  80, 255),
+    VelColor(  0,  26,  90),
+    VelColor(  0,   7,  25),
+    VelColor( 70,  71, 255),
+    VelColor(  0,   0, 255),  #  45: blue
+    VelColor(  0,   0,  91),
+    VelColor(  0,   0,  25),
+    VelColor(131,  71, 255),
+    VelColor( 80,   0, 255),
+    VelColor( 22,   0, 103),  #  50
+    VelColor( 11,   0,  50),
+    VelColor(255,  73, 255),
+    VelColor(255,   0, 255),
+    VelColor( 90,   0,  90),
+    VelColor( 25,   0,  25),
+    VelColor(255,  77, 132),
+    VelColor(255,   7,  82),
+    VelColor( 90,   1,  27),
+    VelColor( 33,   0,  16),
+    VelColor(255,  25,   0),  #  60: red
+    VelColor(155,  53,   0),
+    VelColor(122,  81,   0),
+    VelColor( 62, 100,   0),
+    VelColor(  0,  56,   0),
+    VelColor(  0,  84,  50),
+    VelColor(  0,  83, 126),
+    VelColor(  0,   0, 255),  #  67: blue
+    VelColor(  0,  68,  77),
+    VelColor( 27,   0, 210),
+    VelColor(124, 124, 124),  #  70: grey
+    VelColor( 32,  32,  32),
+    VelColor(255,  10,   0),  #  72: red
+    VelColor(186, 253,   0),
+    VelColor(170, 237,   0),
+    VelColor( 86, 253,   0),
+    VelColor(  0, 136,   0),
+    VelColor(  0, 252, 122),
+    VelColor(  0, 167, 255),
+    VelColor(  0,  27, 255),  #  79: blue
+    VelColor( 53,   0, 255),  #  80
+    VelColor(119,   0, 255),
+    VelColor(180,  23, 126),
+    VelColor( 65,  32,   0),
+    VelColor(255,  74,   0),
+    VelColor(131, 225,   0),
+    VelColor(101, 253,   0),
+    VelColor(  0, 253,   0),  #  87: green
+    VelColor(  0, 253,   0),  #  88: green
+    VelColor( 69, 253,  97),
+    VelColor(  0, 252, 202),  #  90
+    VelColor( 80, 134, 255),
+    VelColor( 39,  77, 201),
+    VelColor(130, 122, 237),
+    VelColor(211,  12, 255),
+    VelColor(255,   6,  90),
+    VelColor(255, 125,   0),
+    VelColor(185, 177,   0),
+    VelColor(138, 253,   0),
+    VelColor(130,  93,   0),
+    VelColor( 57,  40,   0),  # 100
+    VelColor( 13,  76,   5),
+    VelColor(  0,  80,  55),
+    VelColor( 19,  19,  41),
+    VelColor( 16,  31,  90),
+    VelColor(106,  60,  23),
+    VelColor(172,   4,   0),
+    VelColor(225,  81,  53),
+    VelColor(220, 105,   0),
+    VelColor(255, 225,   0),
+    VelColor(153, 225,   0),  # 110
+    VelColor( 95, 181,   0),
+    VelColor( 27,  27,  49),
+    VelColor(220, 253,  84),
+    VelColor(118, 252, 184),
+    VelColor(150, 151, 255),
+    VelColor(139,  97, 255),
+    VelColor( 64,  64,  64),  # 117: grey
+    VelColor(116, 116, 116),  # 118: grey
+    VelColor(222, 252, 252),
+    VelColor(164,   4,   0),  # 120
+    VelColor( 53,   0,   0),
+    VelColor(  0, 209,   0),
+    VelColor(  0,  64,   0),
+    VelColor(185, 177,   0),
+    VelColor( 61,  48,   0),
+    VelColor(180,  93,   0),
+    VelColor( 74,  20,   0)   # 127
+]
--- launchpad_midi.py
+++ launchpad_midi.py
@@ -1,6 +1,20 @@
+"""
+Setup instructions:
+    1. python3 on RPi
+    2. sudo apt install sense-hat
+    3. pip3 install mido python-rtmidi recordclass
+    4. python3 launchpad_midi.py
+
+"""
 import mido
+import platform
 import re
 import random
+from sense_hat import SenseHat
+
+from launchpad_colors import LAUNCHPAD_COLORS
+
+assert platform.python_version_tuple() > ('3', '6', '0')
 
 """
 Midi Through:Midi Through Port-0 14:0
@@ -15,29 +29,51 @@ def select_device(device_regex, mode):
     elif mode == "out":
         names = mido.get_output_names()
 
-    print(names)
     for name in names:
         if re.match(device_regex, name):
-            print(f"Selected {name}")
+            print(f"Selected {name} for {mode}put")
             return name
 
+# Initialize the SenseHat
+sense = SenseHat()
+sense.clear()
+sense.set_rotation(0)  # HDMI port facing down
+sense.low_light = True
 
 LAUNCHPAD_REGEX = r"^Launchpad MK2.*$"
 input_name = select_device(LAUNCHPAD_REGEX, "in")
 output_name = select_device(LAUNCHPAD_REGEX, "out")
 
+_DEFAULT_MIN_VELCOLOR = 48
+MIN_VELCOLOR = _DEFAULT_MIN_VELCOLOR // (2 if sense.low_light else 1)
+
+print("Opening devices...")
 with mido.open_input(input_name) as inport, mido.open_output(output_name) as outport:
+    print("MIDI Loopback enabled")
     while True:
-        # msg = inport.receive()
-        # Manual is one-indexed, mido is zero indexed
+        # Launchpad documentation is one-indexed, mido is zero indexed
         # Channel 1: on
         # Channel 2: flashing
         # Channel 3: pulsing
         msg = inport.receive()
-        print(msg)
 
         if hasattr(msg, 'velocity') and msg.velocity > 0:
             msg = msg.copy(velocity=random.randint(1, 127))
         elif hasattr(msg, 'value') and msg.value > 0:
             msg = msg.copy(value=random.randint(1, 127))
+
         outport.send(msg)
+
+        if re.match("^note_(?:on|off)$", msg.type):
+            #print(msg.note, "on" if msg.velocity > 0 else "off")
+            row = 8 - msg.note // 10
+            col = msg.note % 10 - 1
+
+            color = LAUNCHPAD_COLORS[msg.velocity]
+            color.red = max(color.red, MIN_VELCOLOR) if color.red > 0 else 0
+            color.green = max(color.green, MIN_VELCOLOR) if color.green > 0 else 0
+            color.blue = max(color.blue, MIN_VELCOLOR) if color.blue > 0 else 0
+
+            if row in range(0, 8) and col in range(0, 8):  # range is [a, b) inc/exc
+                sense.set_pixel(col, row, color)  # x=col, y=row
+                print(f"row={row} col={col} color={color}")
