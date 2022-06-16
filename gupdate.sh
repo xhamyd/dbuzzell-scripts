5191a5bbe690588d2ce3fbb773a4066af16bb3c4
David Buzzell <dbuzzell@dspconcepts.com>
2022-06-16 09:28:23 -0700

    init commit

--- gupdate.sh
+++ gupdate.sh
@@ -1,13 +1,13 @@
 RETURN_DIR=$(pwd)
 
-cd "${GIT_DIR}" || echo "ERROR: Cannot enter ${GIT_DIR}" && exit
+cd "${DBUZZELL_GIT_DIR}" || (echo "ERROR: Cannot enter ${DBUZZELL_GIT_DIR}" && exit)
 
 # Source: https://unix.stackexchange.com/a/86724
 for git_dir in */; do
-    cd "${git_dir}" || exit
+    cd "${git_dir}" || (echo "ERROR: Cannot enter ${git_dir}" && exit)
     echo "Pulling ${git_dir}..."
     git pull -q
     cd ..
 done
 
-cd "${RETURN_DIR}" || echo "ERROR: Cannot enter ${RETURN_DIR}" && exit
+cd "${RETURN_DIR}" || (echo "ERROR: Cannot enter ${RETURN_DIR}" && exit)
