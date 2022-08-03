415da011ac4d68156d29f9ef4475ec81b945cd62
David Buzzell <dbuzzell@dspconcepts.com>
2022-08-03 15:31:02 -0700

    fixing bug in gupdate, adding branch name

--- gupdate.sh
+++ gupdate.sh
@@ -1,13 +1,14 @@
 RETURN_DIR=$(pwd)
 
-cd "${DBUZZELL_GIT_DIR}" || echo "ERROR: Cannot enter ${DBUZZELL_GIT_DIR}" && exit
+cd "${DBUZZELL_GIT_DIR}" || (echo "ERROR: Cannot enter ${DBUZZELL_GIT_DIR}" && exit)
 
 # Source: https://unix.stackexchange.com/a/86724
 for git_dir in */; do
     cd "${git_dir}" || exit
-    echo "Pulling ${git_dir}..."
+    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+    echo "Pulling ${git_dir} on ${BRANCH_NAME}..."
     git pull -q
     cd ..
 done
 
-cd "${RETURN_DIR}" || echo "ERROR: Cannot enter ${RETURN_DIR}" && exit
+cd "${RETURN_DIR}" || (echo "ERROR: Cannot enter ${RETURN_DIR}" && exit)
