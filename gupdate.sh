20769fb606dd6c5c27cc5fb57dde43d8b325ef54
David Buzzell <dbuzzell@dspconcepts.com>
2022-08-03 15:40:37 -0700

    robust git check

--- gupdate.sh
+++ gupdate.sh
@@ -5,9 +5,15 @@ cd "${DBUZZELL_GIT_DIR}" || (echo "ERROR: Cannot enter ${DBUZZELL_GIT_DIR}" && e
 # Source: https://unix.stackexchange.com/a/86724
 for git_dir in */; do
     cd "${git_dir}" || exit
-    BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-    echo "Pulling ${git_dir} on ${BRANCH_NAME}..."
-    git pull -q
+
+    if [ ! -d .git ]; then
+	echo "Not a valid git dir: ${git_dir}"
+    else 
+	BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+	echo "Pulling ${git_dir} on ${BRANCH_NAME}..."
+	git pull -q
+    fi
+
     cd ..
 done
 
