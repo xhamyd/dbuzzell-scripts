3f5f6ade5e1cbe14eab970906aeb59323702a0ab
David Buzzell <xhamyd@gmail.com>
2024-05-25 03:00:54 -0700

    More expansions to print playlist data

--- python-docker/Spotify API/src/spotify_api.py
+++ python-docker/Spotify API/src/spotify_api.py
@@ -18,7 +18,7 @@ print("Done!\n")
 # results = sp.current_user_playlists(limit=50)
 # for i, item in enumerate(results['items']):
 #     print("%d %s" % (i, item['name']))
-playlists = sp.user_playlists("xhamyd")
+playlists = sp.user_playlists("xhamyd", limit=100)
 print(f"Found {len(playlists["items"])} playlists...")
 
 for i, playlist in enumerate(playlists['items']):
@@ -27,6 +27,27 @@ for i, playlist in enumerate(playlists['items']):
     print(f"    owner = {playlist['owner']['display_name']}")
     print(f"    public = {playlist['public']}")
     print(f"    collaborative = {playlist['collaborative']}")
+
+    tracks = sp.playlist_items(playlist['id'], fields="items,next", limit=200)
+    info_tracks = list()
+    while tracks:
+        for track in tracks['items']:
+            track_obj = track['track']
+            info_tracks.append(dict(name=track_obj['name'],
+                                    artists=", ".join(artist['name'] for artist in track_obj['artists']),
+                                    album=track_obj['album']['name']))
+
+        # Queries are limited, so keep querying until we have exhausted the list
+        if tracks['next']:
+            print(f"Found {len(info_tracks)} tracks, still searching...")
+            tracks = sp.next(tracks)
+        else:
+            tracks = None
+
+    print("=== Tracks: ===")
+    # TODO: write data to file directly
+    for j, track in enumerate(info_tracks):
+        print(f"{j}: \"{track['name']}\" by {track['artists']} ({track['album']})")
     print()
 
-print("\nDone!\n")
+print("Done!\n")
